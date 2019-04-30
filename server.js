5de939597408295294b630c66e9e000a318b32b6
Jared <jaredbolduc@yahoo.com>
2019-04-29 19:48:36 -0600

    saVER

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
+require('dotenv').config():
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
